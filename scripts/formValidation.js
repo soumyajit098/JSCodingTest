04b70b791d232d846b93417d8cd0ec5d3842f3ab
Soumyajit Pal <soumyajit.pal@zensar.com>
2020-07-27 20:11:33 +0530

    Add a file.

---
+++ scripts/formValidation.js
@@ -0,0 +1,33 @@
+let btn = document.getElementById('btnsubmit')
+
+function onBtnClick(){
+    let userName = document.getElementById('uname').value
+let email = document.getElementById('email').value
+let pass = document.getElementById('pass').value
+let cpass = document.getElementById('cpass').value
+    if(userName.value=='NULL' || userName.value.length<4){
+        div.innerHTML='Username must be3 atleast 3 characters'
+    }
+    let mailformat = /^w+([.-]?w+)@w+([.-]?w+)(.w{2,3})+$/;
+    if(email!=null)
+    {
+        if(email.value.match(mailformat))
+        {
+        return true;
+        }
+        else{
+            div.innerHTML= 'Email is not valid'
+
+        }
+    }
+    if(pass==null || pass.length<=6){
+        div.innerHTML='Password must be atleast 6 characters'
+    }
+    if(cpass!==pass || cpass===null){
+        div.innerHTML='password2 is required'
+    }
+    if(userName=='NULL' || userName.length<4){
+        div.innerHTML='Username must be3 atleast 3 characters'
+    }
+}
+btnsubmit.addEventListener('click',onBtnClick)
